d1ffa012dcfc43c244e806b9834bb4ba4c9abdfe
Your Name <youremail@yourdomain.com>
2024-01-06 22:42:44 +0300

    Bug fixes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
 })
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 ScrollSmoother.create({
